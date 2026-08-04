5708edf64c61c9b782feecb5299bc0681688fcac
thaweesak2504 <davide.twt&gmail.com>
2026-08-04 23:12:37 +0700

    Fix Tailwind config ESLint errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import typography from '@tailwindcss/typography';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -81,6 +83,6 @@ export default {
     },
   },
   plugins: [
-    require('@tailwindcss/typography'),
+    typography,
   ],
 }
